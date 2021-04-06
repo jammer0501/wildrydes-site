eba241d0e21f7978de4e5483503811b293d02f84
jammer0501 <james.killick@btinternet.com>
2021-04-06 16:35:21 +0100

    added invoke url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://ef1hx7yqm6.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
