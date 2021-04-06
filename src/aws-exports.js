b783401628f62f5d82d7f59c16daad751fff7ae4
jammer0501 <james.killick@btinternet.com>
2021-04-06 14:26:36 +0100

    desperation

---
+++ src/aws-exports.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_O0pD1aJhW', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '2dicurrg2j58ml8mtr8cavla4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
