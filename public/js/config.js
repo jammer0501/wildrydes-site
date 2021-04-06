dfb4f99833d582eea7bd4739c920a887248fea18
jammer0501 <james.killick@btinternet.com>
2021-04-06 11:50:52 +0100

    added Cognito config

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'eu-west-2_O0pD1aJhW', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '2dicurrg2j58ml8mtr8cavla4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
